24ecd667881f9f5ffedc7c1b8d6cbb7a42b09741
rodrigodeleon <ro.dleon@hotmail.com>
2016-08-18 07:59:33 -0300

    probando cosas con heroku

--- utils/database_connection.js
+++ utils/database_connection.js
@@ -2,7 +2,7 @@ var pg = require('pg');
 
 module.exports = {
   connect: function () {
-    var connectionString = process.env.DATABASE_URL || 'postgres://postgres:rodrigo1@localhost:5432/prueba';
+    var connectionString = process.env.DATABASE_URL ||  'postgres://dgvyrdhkydexfb:OxADObHgjNI_t_sBJ6CKdcKkw1@ec2-54-225-91-215.compute-1.amazonaws.com:5432/dctfujbpbd52ir'; //conexion para servidor local 'postgres://postgres:rodrigo1@localhost:5432/prueba';
     var client = new pg.Client(connectionString);
     client.connect();
     return client;
